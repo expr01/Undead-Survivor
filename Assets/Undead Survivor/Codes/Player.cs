eabaeb7f888d2713f3502ee2723568bc2e1aabff
expr <jeje300011@gmail.com>
2024-10-24 21:56:40 +0900

    플레이어 애니메이션 생성
    플레이어 애니메이션 생성

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
-    public float speed;
+    public float speed = 3.0f;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,5 +30,15 @@ public class Player : MonoBehaviour
         // normalized를 통해 모든 방향으로 크기 1 고정, fixedDeltaTime은 물리프레임 하나가 소비한 시간
         Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0; // 좌측 키 눌렀을 때 true
+        }
+
     }
 }
